0a4577dd7aeb563f04a9e55d2258308a4a77af98
Julia Frederico <magalhaesjulia@id.uff.br>
2023-02-06 16:44:43 -0300

    chore: run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_06_192237) do
+ActiveRecord::Schema.define(version: 2023_02_06_193351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "street"
+    t.string "neighborhood"
+    t.string "city"
+    t.string "state"
+    t.string "complement"
+    t.string "reference"
+    t.string "zipcode"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +44,5 @@ ActiveRecord::Schema.define(version: 2023_02_06_192237) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
 end
