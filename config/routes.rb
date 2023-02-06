c15081622ff7a2c2335aefaf78a441c629c1a5a8
Julia Frederico <magalhaesjulia@id.uff.br>
2023-02-06 17:00:01 -0300

    feat: add static pages for home and about

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'registrations' }
 
-  root to: "home#index"
+  root to: 'pages#home'
+
+  get '/sobre', to: 'pages#about'
 
 end
