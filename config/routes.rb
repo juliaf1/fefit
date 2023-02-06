ac5fe8cf57083a7190ea6c32d2d9f860dbb7f1d8
Julia Frederico <magalhaesjulia@id.uff.br>
2023-02-06 17:12:16 -0300

    chore: rails g administrate install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+      resources :users
+      resources :clients
+      resources :super_admins
+      resources :addresses
+
+      root to: "users#index"
+    end
   devise_for :users, controllers: { registrations: 'registrations' }
 
   root to: 'pages#home'
