67ed8a3bafb9850282688ffb746c1015bc677841
Julia Frederico <magalhaesjulia@id.uff.br>
2023-02-06 22:54:49 -0300

    feat: add validate zipcode methods

--- app/services/location_manager/zip_code_area_validator.rb
+++ app/services/location_manager/zip_code_area_validator.rb
@@ -1,7 +1,9 @@
 module LocationManager
   class ZipCodeAreaValidator
 
-    AVAILABLE_LOCATIONS = [
+    API_BASE_URL = 'https://brasilapi.com.br/api/cep/v1'
+
+    AVAILABLE_LOCATIONS = {
       buzios: {
         name: 'Armação dos Búzios',
         zipcode: '289',
@@ -12,30 +14,94 @@ module LocationManager
         zipcode: '279',
         available_areas: ['', '',]
       }
-    ]
+    }
 
     def initialize(zipcode)
       @zipcode = clean_zipcode(zipcode)
     end
 
     def call
+      validate_area
     end
 
     private
 
     def validate_area
-      if @zipcode == AVAILABLE_LOCATIONS.buzios.default_zip_code
-        return { success: true }
-      elsif @zipcode.first(3) == AVAILABLE_LOCATIONS.buzios.zipcode
-        validate_zipcode
-      elsif @zipcode.first(3) == AVAILABLE_LOCATIONS.macae.zipcode
-        validate_zipcode
+      case @zipcode.first(3)
+      when AVAILABLE_LOCATIONS[:buzios][:zipcode]
+        return validate_buzios_zipcode
+      when AVAILABLE_LOCATIONS[:macae][:zipcode]
+        return validate_macae_zipcode
       else
-        return { success: false }
+        return {
+          success: false
+        }
       end
     end
 
-    def validate_zipcode
+    def validate_macae_zipcode
+      response = HTTParty.get("#{API_BASE_URL}/#{@zipcode}")
+
+      if response.code == 200
+        address = response.body
+
+        if AVAILABLE_LOCATIONS[:macae][:available_areas].include?(address.neighborhood)
+          return {
+            success: true,
+            address: {
+              zipcode: address.cep,
+              state: address.state,
+              city: address.city,
+              neighborhood: address.neighborhood,
+              street: address.street,
+            }
+          }
+        else
+          return {
+            success: false
+          }
+        end
+      else
+        return {
+          success: false
+        }
+      end
+    end
+
+    def validate_buzios_zipcode
+      if @zipcode == AVAILABLE_LOCATIONS[:buzios][:default_zip_code]
+        return {
+          success: true,
+          address: {
+            zipcode: zipcode,
+            state: 'RJ',
+            city: 'Armação dos Búzios',
+            neighborhood: 'Búzios - CEP Geral',
+          }
+        }
+      end
+
+      response = HTTParty.get("#{API_BASE_URL}/#{@zipcode}")
+
+      puts response
+  
+      if response.code == 200
+        address = response.body
+        return {
+          success: true,
+          address: {
+            zipcode: address.cep,
+            state: address.state,
+            city: address.city,
+            neighborhood: address.neighborhood,
+            street: address.street,
+          }
+        }
+      else
+        return {
+          success: false
+        }
+      end
     end
 
     def clean_zipcode(zipcode)
