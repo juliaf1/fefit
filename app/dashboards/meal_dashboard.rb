ab5da182a74d8b336aaad7f817482974188bc4b1
Julia Frederico <magalhaesjulia@id.uff.br>
2023-02-07 13:05:00 -0300

    feat: update meal dashboard properties order

--- app/dashboards/meal_dashboard.rb
+++ app/dashboards/meal_dashboard.rb
@@ -28,23 +28,23 @@ class MealDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
     id
-    description
-    hot
     name
+    description
+    units
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
     id
-    description
-    hot
     name
-    photo_url
+    description
     price
+    photo_url
     units
-    vegan
     veggie
+    vegan
+    hot
     created_at
     updated_at
   ].freeze
@@ -53,14 +53,14 @@ class MealDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    description
-    hot
     name
-    photo_url
+    description
     price
+    photo_url
     units
-    vegan
     veggie
+    vegan
+    hot
   ].freeze
 
   # COLLECTION_FILTERS
